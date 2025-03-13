01d64352d2f3dfb6cd4dbf19984e60028185fc8c
Hanno <nodscher@icloud.com>
2025-03-13 22:38:29 +0100

    add comments

--- build.sh
+++ build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -e
 
+# make upload area
 mkdir packages
 
-
+# build mesa
 cd mesa
 makepkg -s --noconfirm
-
+# move packages to upload area
 rm *debug*.pkg.tar.zst
 mv *.pkg.tar.zst ../packages
 
-
+# build lib32-mesa
 cd ../lib32-mesa
 makepkg -s --noconfirm
-
+# move packages to upload area
 rm *debug*.pkg.tar.zst
 mv *.pkg.tar.zst ../packages
