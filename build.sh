d8041e9ff2a899cfd9c2783ad555c608dbfdbb8c
Hanno <nodscher@icloud.com>
2025-03-13 22:36:43 +0100

    compile lib32 and upload artifacts

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
-whoami
+#!/bin/bash
+mkdir packages
+
+
 cd mesa
 makepkg -s --noconfirm
-ls -la
+
+rm *debug*.pkg.tar.zst
+mv *.pkg.tar.zst ../packages
+
+
+cd ../lib32-mesa
+makepkg -s --noconfirm
+
+rm *debug*.pkg.tar.zst
+mv *.pkg.tar.zst ../packages
