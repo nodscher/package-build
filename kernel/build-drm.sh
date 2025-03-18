f9d2a57804333f072f201101a8fecc79dac442e9
Hanno <nodscher@icloud.com>
2025-03-19 00:56:03 +0100

    fix linux drm sh

--- kernel/build-drm.sh
+++ kernel/build-drm.sh
@@ -4,8 +4,8 @@ set -e
 # make upload area
 mkdir packages
 
-#build linux-mainline
-cd linux-mainline
+#build linux-drm
+cd linux-drm
 grep CONFIG_NTSYNC=m config || echo CONFIG_NTSYNC=m | tee -a config 
 updpkgsums
 makepkg -s --noconfirm
