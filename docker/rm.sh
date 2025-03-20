e9a9c311cd04af237bbe7eb03b01ec0d92d04a18
Hanno Repnak <nodschers@icloud.com>
2025-03-21 00:00:26 +0100

    fix rm cmd

--- docker/rm.sh
+++ docker/rm.sh
@@ -4,6 +4,6 @@ set -e
 cd /buildzone/repo/custom
 for item in $(find -name '*.pkg.tar.zst' -mtime +6 | sed -E 's#^\./##; s/(.*)-x86_64.pkg.tar.zst/\1/'); do 
     if [[ "$(tar --exclude='*/*' -tf custom.db.tar.zst | tr : _)" != *"$item"* ]]; then
-        rm -f "$item"
+        rm -vf "$item"-x86_64.pkg.tar.zst
     fi
 done
