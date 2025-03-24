8ea02448a2c85ce506f92e9e8bfa8bb3c253300d
Hanno <nodscher@icloud.com>
2025-03-24 17:59:23 +0100

    get mailine-ver remotely

--- docker/build_pacman.sh
+++ docker/build_pacman.sh
@@ -18,7 +18,8 @@ elif [[ $(basename $0) == build-linux-drm ]]; then
     latest=$(git ls-remote https://gitlab.freedesktop.org/drm/tip.git HEAD | cut -c1-9)
     [[ "$(tar --exclude='*/*' -tf $STARTDIR/repo/custom/custom.db.tar.zst | grep linux-drm-tip-git-)" != *"$latest"* ]] || (echo linux-drm-tip-git-$latest is already up to date; exit 1)
 elif [[ $(basename $0) == build-linux-mainline ]]; then
-    latest=$(cat kernel/linux-mainline/PKGBUILD | grep pkgver= | cut -d= -f2)
+    #latest=$(cat kernel/linux-mainline/PKGBUILD | grep pkgver= | cut -d= -f2)
+    latest=$(curl -s "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=linux-mainline" | sed -n -e 's/^pkgver=//p' -e 's/^pkgrel=//p' | paste -sd-)
     [[ "$(tar --exclude='*/*' -tf $STARTDIR/repo/custom/custom.db.tar.zst | grep linux-mainline)" != *"$latest"* ]] || (echo linux-mainline-$latest is already up to date; exit 1)
 else
     echo "Unknown function"
