800a725bdac262977a7f3f3adfacf10390c73240
Hanno <nodscher@icloud.com>
2025-04-10 19:16:39 +0200

    fix up-to-date names

--- docker/build_pacman.sh
+++ docker/build_pacman.sh
@@ -11,13 +11,13 @@ elif [[ $(basename $0) == build-linux-drm ]]; then
     [[ "$(tar --exclude='*/*' -tf $STARTDIR/repo/custom/custom.db.tar.zst | grep linux-drm-tip-git-)" != *"$latest"* ]] || (echo linux-drm-tip-git-$latest is already up to date; exit 1)
 elif [[ $(basename $0) == build-linux-amd ]]; then
     latest=$(git ls-remote https://gitlab.freedesktop.org/agd5f/linux.git drm-next | cut -c1-9)
-    [[ "$(tar --exclude='*/*' -tf $STARTDIR/repo/custom/custom.db.tar.zst | grep linux-amd-)" != *"$latest"* ]] || (echo linux-drm-tip-git-$latest is already up to date; exit 1)
+    [[ "$(tar --exclude='*/*' -tf $STARTDIR/repo/custom/custom.db.tar.zst | grep linux-amd-git-)" != *"$latest"* ]] || (echo linux-amd-git-$latest is already up to date; exit 1)
 elif [[ $(basename $0) == build-linux-mainline ]]; then
     latest=$(curl -s "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=linux-mainline" | sed -n -e 's/^pkgver=//p' -e 's/^pkgrel=//p' | paste -sd-)
     [[ "$(tar --exclude='*/*' -tf $STARTDIR/repo/custom/custom.db.tar.zst | grep linux-mainline)" != *"$latest"* ]] || (echo linux-mainline-$latest is already up to date; exit 1)
 elif [[ $(basename $0) == build-linux-amd-drm-next ]]; then
     latest=$(curl -s "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=linux-amd-drm-next" | sed -n -e 's/^pkgver=//p' -e 's/^pkgrel=//p' | paste -sd-)
-    [[ "$(tar --exclude='*/*' -tf $STARTDIR/repo/custom/custom.db.tar.zst | grep linux-amd-drm-next)" != *"$latest"* ]] || (echo linux-mainline-$latest is already up to date; exit 1)
+    [[ "$(tar --exclude='*/*' -tf $STARTDIR/repo/custom/custom.db.tar.zst | grep linux-amd-drm-next)" != *"$latest"* ]] || (echo linux-amd-drm-next-$latest is already up to date; exit 1)
 else
     echo "Unknown function"
     exit 1
